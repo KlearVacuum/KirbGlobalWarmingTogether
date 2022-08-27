3b5668edb1e68f4f4fe5ce3bcacc5640b81874da
Yumi <crystal63030@yahoo.com.sg>
2022-08-28 00:01:55 +0800

    Update TrashScript.cs

--- Kirb Global Warming Together Unity/Assets/Scripts/TrashScript.cs
+++ Kirb Global Warming Together Unity/Assets/Scripts/TrashScript.cs
@@ -6,9 +6,26 @@ public class TrashScript : MonoBehaviour
 {
     public eTrashType trashType;
     public int trashCash;
+
+    public bool _isYumisTrash;
+
+    private Vector3 _current, _target, velocity;
+    [SerializeField] private float _dampRatio = 1.0f, _angular = 0.3f, _timeStep = 0.5f;
+
     void Start()
     {
         GlobalGameData.AddTrash(gameObject);
+    }
+
+    void Update()
+    {
+        if (_isYumisTrash)
+        {
+            _current = transform.position;
+            _target = GameObject.FindObjectOfType<AIEntity>().transform.position;
+            SpringMath.Lerp(ref _current, ref velocity, _target, _dampRatio, _angular, _timeStep);
+            transform.position = _current;
+        }
     }
 
     public void RemoveTrash()
