06d55da044b25f57d15ed310e4b5beaad940ef8b
Yumi <crystal63030@yahoo.com.sg>
2022-08-28 13:52:41 +0800

    Update AlertSpeakerObject.cs

--- Kirb Global Warming Together Unity/Assets/Scripts/AlertSpeakerObject.cs
+++ Kirb Global Warming Together Unity/Assets/Scripts/AlertSpeakerObject.cs
@@ -22,11 +22,18 @@ public class AlertSpeakerObject : MonoBehaviour
 
     public void DoTheWeeWoo()
     {
+        LeanTween.cancel(gameObject);
+
         LeanTween
             .rotateX(gameObject, 0.0f, 0.5f)
             .setEaseOutBack();
         LeanTween
             .delayedCall(0.5f, () => {
-                LeanTween.scaleY(gameObject, 0.225f, 0.25f).setLoopPingPong(3); });
+                LeanTween.scaleY(gameObject, 0.225f, 0.25f).setLoopPingPong(5)
+                    .setOnComplete(() => {
+                        LeanTween
+                            .rotateX(gameObject, 90.0f, 0.5f)
+                            .setEaseInBack();
+                    }); });
     }
 }
