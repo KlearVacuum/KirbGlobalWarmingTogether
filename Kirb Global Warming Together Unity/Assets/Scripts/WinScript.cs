ad8df729a784f4cd5912d0bde181ee9dbd13d34b
Yumi <crystal63030@yahoo.com.sg>
2022-08-28 17:48:51 +0800

    Pushes

--- Kirb Global Warming Together Unity/Assets/Scripts/WinScript.cs
+++ Kirb Global Warming Together Unity/Assets/Scripts/WinScript.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WinScript : MonoBehaviour
 {
     public int winCost;
     private int timesInvested;
     public int timesInvestedToWin;
+    public Image _fillImage;
+    public GameObject _finalBoi;
 
     public void BuyWin()
     {
@@ -15,8 +18,13 @@ public class WinScript : MonoBehaviour
             GlobalGameData.cash -= winCost;
             timesInvested++;
 
+            _fillImage.fillAmount = (float)timesInvested / timesInvestedToWin;
+            Debug.Log(_fillImage.fillAmount);
+
             if (timesInvested >= timesInvestedToWin)
             {
+                var go = Instantiate(_finalBoi);
+                go.GetComponent<Animator>().CrossFade("kirbIG_xtra", 0, 0);
                 LevelManager.Instance.NotifyWin();
             }
         }
